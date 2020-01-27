e8195fe1739c29e7a76fcc2796cc6ae590bfd48c
andrejs.dasko <andrejs.dasko@gmail.com>
2020-01-27 23:37:29 +0200

    Initial commit

---
+++ src/main/java/com/clockifyapipoc/ClockifyApiPocApplication.java
@@ -0,0 +1,13 @@
+package com.clockifyapipoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockifyApiPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockifyApiPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clockifyapipoc/ClockifyApiPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clockifyapipoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockifyApiPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
